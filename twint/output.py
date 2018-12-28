d9eec68c483bf1bb52bed2c4d0482a49f94b5841
aseemshrey <aseemshrey@gmail.com>
2018-12-28 17:28:31 +0530

    Check for copyright tweet when writing to DB

--- twint/output.py
+++ twint/output.py
@@ -91,18 +91,18 @@ async def checkData(tweet, location, config, conn):
 
     if datecheck(tweet.datestamp, config):
         output = format.Tweet(config, tweet)
+        if copyright is None and is_tweet(tweet):
+            if config.Database:
+                db.tweets(conn, tweet, config)
 
-        if config.Database:
-            db.tweets(conn, tweet, config)
+            if config.Pandas:
+                panda.update(tweet, config)
 
-        if config.Pandas:
-            panda.update(tweet, config)
+            if config.Store_object:
+                tweets_object.append(tweet)
 
-        if config.Store_object:
-            tweets_object.append(tweet)
-
-        if config.Elasticsearch:
-            elasticsearch.Tweet(tweet, config)
+            if config.Elasticsearch:
+                elasticsearch.Tweet(tweet, config)
 
         _output(tweet, output, config)
 
